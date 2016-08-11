19daf7b71197772d877b0edd74b1860ed50a34c6
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-10 19:13:13 -0500

    added test for even or odd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // if a number is odd or even.  If the number is evelnly divisible
 // by 2, it is an even number.  A remainder indicates if it is odd.
 function myFunction() {
-    var x, text;
+    var x, text, oddOrEven;
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
@@ -10,8 +10,14 @@ function myFunction() {
     // If x is Not a Number or less than one or greater than 10
     if (isNaN(x) || x < 1 || x > 100) {
         text = "Input not valid";
+        oddOrEven = "";
     } else {
-        text = "Input OK";        
+        text = "Input OK";
+        if(x % 2 === 0)
+          oddOrEven = "Is even";
+        else
+          oddOrEven = "Is odd";
     }
     document.getElementById("demo").innerHTML = text;
+    document.getElementById("demo1").innerHTML = oddOrEven;
 }
